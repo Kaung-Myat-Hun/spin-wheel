62ce65e75f182b4992f84200ab0d7529b764c6c6
Kaung Myat Hun <kaungmyathun7@gmail.com>
2023-02-27 04:38:23 +0630

    adding: features

--- src/components/SpinWheels/SpinWheels.jsx
+++ src/components/SpinWheels/SpinWheels.jsx
@@ -1,47 +1,64 @@
-import React from "react";
-import styles from "./SpinWheels.module.css";
+import React, { useState } from "react";
+import "./SpinWheels.css";
 
 function SpinWheels() {
+  const [name, setname] = useState({ name: "circle" });
+  const [loading, setLoading] = useState(false);
+  const startRotation = () => {
+    setname({ name: "circle startRotate" });
+    setLoading(true);
+    setTimeout(() => {
+      setname({ name: "circle startRotate stopRotate" });
+      setLoading(false);
+    }, Math.floor(Math.random() * 10000) + 1);
+  };
   return (
     <div>
-      <ul className={styles.circle}>
+      <div className="arrow"></div>
+      <ul className={name.name}>
         <li>
-          <div className={styles.text}>1</div>
+          <div className="text">1</div>
         </li>
         <li>
-          <div className={styles.text}>2</div>
+          <div className="text">2</div>
         </li>
         <li>
-          <div className={styles.text}>3</div>
+          <div className="text">3</div>
         </li>
         <li>
-          <div className={styles.text}>4</div>
+          <div className="text">4</div>
         </li>
         <li>
-          <div className={styles.text}>5</div>
+          <div className="text">5</div>
         </li>
         <li>
-          <div className={styles.text}>6</div>
+          <div className="text">6</div>
         </li>
         <li>
-          <div className={styles.text}>7</div>
+          <div className="text">7</div>
         </li>
         <li>
-          <div className={styles.text}>8</div>
+          <div className="text">8</div>
         </li>
         <li>
-          <div className={styles.text}>9</div>
+          <div className="text">9</div>
         </li>
         <li>
-          <div className={styles.text}>10</div>
+          <div className="text">10</div>
         </li>
         <li>
-          <div className={styles.text}>11</div>
+          <div className="text">11</div>
         </li>
         <li>
-          <div className={styles.text}>12</div>
+          <div className="text">12</div>
         </li>
       </ul>
+      <button
+        className={loading ? "disableBtn" : "spinBtn"}
+        onClick={startRotation}
+        disabled={loading ? true : false}>
+        {loading ? "Spinning..." : "Spin"}
+      </button>
     </div>
   );
 }
